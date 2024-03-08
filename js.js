2f48c86b7f1938e76c683168de09297a7241bc41
mohamed mahmoud <fudy234lg@gmail.com>
2024-03-08 14:53:03 +0200

    upload files

---
+++ js.js
@@ -0,0 +1,62 @@
+// // 1- Calculate the sum of numbers within an array with 5 element
+
+// let num=[2,3,5,10];
+// let sum =0;
+// for(let i=0; i<num.length;i++){
+//     sum+=num[i];
+// }
+// console.log(sum)
+
+
+
+
+// // 2-Using built in Function Remove the spaces found in a string
+
+// let greeting = '   Hello world!   ';
+
+// console.log(greeting);
+
+// console.log(greeting.trim());
+
+
+// // 3-Return a Boolean if a number is divisible by 10
+
+// function DivisibleBy_10(number) {
+//     return number % 10 == 0;
+//   }
+//   console.log(DivisibleBy_10(30));
+  
+//   console.log(Math.max(1, 3, 5, 7)); 
+
+
+//         // part 2
+
+// //  1-what is the data type of "Hello world" 
+// //  answer q 1      "streeng"
+
+
+
+
+// let x="mohamed"+22;
+// console.log(x);
+ 
+
+
+// 3
+ 
+// If the expression  the  && is falsy, it will immediately return false without checking the expression 
+
+
+
+// const a=[2,32,3,5,2]
+// let [...d] =a;
+// c =10
+// console.log(d)
+
+
+
+// The logical AND( && ) operator checks whether all operands are true – the result is true only when all operands are true 
+// using spreed syntax 
+
+
+
